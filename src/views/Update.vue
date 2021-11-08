c03162b9cbec7de614e83cccde9d785ec70a4bd3
Med-Amine-Louati <medamine505@live.fr>
2021-11-08 19:27:50 +0100

    add design for form

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-        <form>
-                <input type="text" name="firstName" placeholder="Enter Name" v-model="user.firstName"/>
-         <input type="text" name="lastame" placeholder="Enter Last Name" v-model="user.lastname"/>
-          <input type="email" name="email" placeholder="Enter email" v-model="user.email"/>
-          <button v-on:click="updateuser">Update user</button>
-        </form>
+     
+                <div class="form-div">
+<h1 id="title">Update user</h1>
+
+<form id="survey-form">
+  <label for="name" id="name-label">First Name </label><br>
+  <input type="text" id="name" name="name" placeholder="Enter your First name" required v-model="user.firstName"><br>
+  <label for="name" id="name-label">Last Name </label><br>
+  <input type="text" id="name" name="name" placeholder="Enter your Last name" required v-model="user.lastname"><br>
+  <label for="email" id="email-label">Email</label><br>
+  <input type="email" id="email" name="email" placeholder="Enter your Email" required v-model="user.email"><br>
+ 
+  <input v-on:click="adduser" type="submit" id="submit" value="Update user">
+  
+</form>
+</div>
     </div>
 </template>
 
@@ -47,3 +57,57 @@ methods:{
 
 
 </script>
+<style>
+html,body {
+  min-height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.form-div {
+  height: 100%;
+  background-image: linear-gradient(rgba(10,80,200,0.5), rgba(0,255,255,0.4)), url("https://www.jotform.com/uploads/ZIGRAM/form_files/eeeee.5eeb483b076531.72239553.jpg");
+  background-size: 100%;
+}
+.form-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+#survey-form {
+  background: rgba(0, 0, 0, 0.31);
+  padding: 3% 10%;
+  border-radius: 2px;
+  display: flex;
+  flex-direction: column;
+  font-size: 3vmin;
+  letter-spacing: 1px;
+}
+label {
+  color: #fcfcfc;
+}
+h1, p {
+  color: #fcfcfc;
+  letter-spacing: 1px;
+}
+p {
+  margin-top: 0px;
+  text-shadow: 3px 3px 8px #005ab3;
+}
+input, select, textarea {
+  padding: 3% 8%;
+  border-radius: 4px;
+  border-color: #fff;
+  border-style: none;
+}
+input[type="submit"] {
+  background-color: #37af65;
+  color: #fff;
+  font-size: 4vmin;
+  letter-spacing: 0px;
+}
+input[type="submit"]:hover {
+  background-color: #2b884f;
+}
+
+</style>
